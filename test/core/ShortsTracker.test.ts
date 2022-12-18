1185fedc9d56870c9824e5ffda1c958c6ea40ead
dreamDev525 <dreamdev525@gmail.com>
2022-12-18 11:08:37 -0800

    shortsTracker test finished

---
+++ test/core/ShortsTracker.test.ts
@@ -0,0 +1,95 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {
+  Token,
+  Vault,
+  Router,
+  Vault__factory,
+  Router__factory,
+  VaultPriceFeed__factory,
+  USDG,
+  VaultPriceFeed,
+  USDG__factory,
+  PancakePair__factory,
+  ShortsTracker,
+  ShortsTracker__factory,
+} from "../../types";
+import { deployments } from "hardhat";
+import chai from "chai";
+import { solidity } from "ethereum-waffle";
+import { reportGasUsed, Ship, toChainlinkPrice, toUsd, toWei } from "../../utils";
+import { PriceFeed } from "types";
+import { Wallet } from "ethers";
+
+chai.use(solidity);
+const { expect } = chai;
+
+let ship: Ship;
+let shortsTracker: ShortsTracker;
+let vault: Vault;
+let eth: Token;
+let btc: Token;
+
+let deployer: SignerWithAddress;
+let user0: SignerWithAddress;
+let user1: SignerWithAddress;
+
+const setup = deployments.createFixture(async (hre) => {
+  ship = await Ship.init(hre);
+  const { accounts, users } = ship;
+  await deployments.fixture(["vault", "shortsTracker"]);
+
+  return {
+    ship,
+    accounts,
+    users,
+  };
+});
+
+describe.only("ShortsTracker", function () {
+  beforeEach(async function () {
+    const { accounts, users } = await setup();
+
+    deployer = accounts.deployer;
+    user0 = users[0];
+    user1 = users[1];
+
+    vault = await ship.connect(Vault__factory);
+    shortsTracker = await ship.connect(ShortsTracker__factory);
+    eth = (await ship.connect("eth")) as Token;
+    btc = (await ship.connect("btc")) as Token;
+    await shortsTracker.setHandler(user0.address, true);
+  });
+
+  it("inits", async function () {
+    expect(await shortsTracker.gov()).to.eq(deployer.address);
+    expect(await shortsTracker.vault()).to.eq(vault.address);
+  });
+
+  it("setIsGlobalShortDataReady", async function () {
+    expect(await shortsTracker.isGlobalShortDataReady()).to.be.false;
+
+    await expect(shortsTracker.connect(user1).setIsGlobalShortDataReady(true)).to.be.revertedWith(
+      "Governable: forbidden",
+    );
+
+    await shortsTracker.setIsGlobalShortDataReady(true);
+    expect(await shortsTracker.isGlobalShortDataReady()).to.be.true;
+
+    await shortsTracker.setIsGlobalShortDataReady(false);
+    expect(await shortsTracker.isGlobalShortDataReady()).to.be.false;
+  });
+
+  it("setInitData", async function () {
+    await expect(shortsTracker.connect(user1).setInitData([], [])).to.be.revertedWith(
+      "Governable: forbidden",
+    );
+
+    expect(await shortsTracker.globalShortAveragePrices(eth.address)).to.eq(0);
+    expect(await shortsTracker.globalShortAveragePrices(btc.address)).to.eq(0);
+
+    shortsTracker.setInitData([eth.address, btc.address], [100, 200]);
+
+    expect(await shortsTracker.globalShortAveragePrices(eth.address)).to.eq(100);
+    expect(await shortsTracker.globalShortAveragePrices(btc.address)).to.eq(200);
+  });
+});
